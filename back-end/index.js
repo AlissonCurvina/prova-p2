bc36ca7169ddec7cee8f36cae6b7d1c4324d2fa4
Alisson Curvina <alisson.curvina@gmail.com>
2025-11-19 21:59:32 -0300

    feat(p2): cria back end e adiciona endpoints

---
+++ back-end/index.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const axios = require('axios')
+
+app.get('/getPOTD', async (req, res) => {
+    const potdClient = axios.create({
+        baseURL: 'https://api.nasa.gov/planetary/apod'
+    })
+
+    const result = await potdClient.get('', {
+        params: {
+            api_key: process.env.NASA_API_KEY
+        }
+    })
+
+    res.json(result.data)
+})
+
+app.get('/searchPhotos', async (req,res) => {
+    const imageLibClient = axios.create({
+        baseURL: 'https://images-api.nasa.gov/search'
+    })
+
+    const result = await imageLibClient.get('', {
+        params: {
+            q: req.query.q 
+        }
+    })
+
+    const photos = result.data.collection.items.filter((item) => {
+        currentDate = new Date(item.data[0].date_created)
+        photoYearDate = currentDate.getFullYear()
+
+        return photoYearDate.toString() ===  req.query.year
+    })
+
+    const filteredPhotos = []
+    let maxPhotos = 10
+
+    maxPhotos = photos.length < maxPhotos ? photos.length : maxPhotos
+    
+    for(let i = 0; i < maxPhotos; i++) {
+        filteredPhotos.push(photos[i])
+    }
+    
+    res.json(filteredPhotos)
+})
+
+const port = 3000
+app.listen(port, () => console.log(`BackPorta ${port}`))
