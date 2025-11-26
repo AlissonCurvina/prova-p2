9daf102e337833440ea50a537190e0909d2e17ad
Alisson Curvina <alisson.curvina@gmail.com>
2025-11-25 23:37:41 -0300

    style(p2): adiciona melhor estilizacao ao app

--- prova-p2/App.js
+++ prova-p2/App.js
@@ -1,21 +1,24 @@
 import { Pressable, StyleSheet, Text, View, FlatList, Linking, Image } from 'react-native'
-import { Button, TextInput } from 'react-native-web'
+import { Button, ScrollView, TextInput } from 'react-native-web'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function App() {
-  const anos = [2024,2023,2022,2021,2020]
+  const anos = [
+    2024,2023,2022,2021,2020
+  ]
+
   const devs = [
     {name: 'Alisson Curvina', linkedin: 'https://www.linkedin.com/in/alisson-curvina', github: 'https://github.com/AlissonCurvina', profileImage: './prova-p2/assets/user.png'},
     {name: 'Evilly Costa', linkedin: 'https://www.linkedin.com/in/evilly-nascimento-costa/', github: 'https://github.com/EvillyCosta', profileImage: './prova-p2/assets/user.png'}
   ]
 
   const photoOfTheDay = {
-      imageDate: "2022-11-19",
-      imageUri: encodeURI("https://images-assets.nasa.gov/video/Moon and Saturn/Moon and Saturn~large.jpg"),
-      imageTitle: "Orion Sees the Moon and Saturn"
-    }
+    imageDate: "2022-11-19",
+    imageUri: encodeURI("https://images-assets.nasa.gov/video/Moon and Saturn/Moon and Saturn~large.jpg"),
+    imageTitle: "Orion Sees the Moon and Saturn"
+  }
 
   const photoOfTheDayList = [
     {
@@ -27,6 +30,11 @@ export default function App() {
       imageDate: "2022-11-21",
       imageUri: encodeURI("https://images-assets.nasa.gov/video/Moon Below Orion art001m1013251756/Moon Below Orion art001m1013251756~large.jpg"),
       imageTitle: "Flight Day 6: Orion Focuses on Moon"
+    },
+    {
+      imageDate: "2022-03-28",
+      imageUri: encodeURI("https://images-assets.nasa.gov/video/NASA Explores the Moon and Beyond/NASA Explores the Moon and Beyond~large.jpg"),
+      imageTitle: "NASA Explores the Moon and Beyond"
     },
     {
       imageDate: "2022-03-28",
@@ -50,168 +58,216 @@ export default function App() {
       imageDate: "2022-03-28",
       imageUri: encodeURI("https://images-assets.nasa.gov/video/NASA Explores the Moon and Beyond/NASA Explores the Moon and Beyond~large.jpg"),
       imageTitle: "NASA Explores the Moon and Beyond"
+    },
+    {
+      imageDate: "2022-03-28",
+      imageUri: encodeURI("https://images-assets.nasa.gov/video/NASA Explores the Moon and Beyond/NASA Explores the Moon and Beyond~large.jpg"),
+      imageTitle: "NASA Explores the Moon and Beyond"
     }
   ]
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.pageName}>Daily Nasa</Text>
-      </View>
-
-      <View style={styles.divider}></View>
-
-      <Text style={styles.heading}>
-        Foto do dia
-      </Text>
-      <Image
-        source={{ uri: photoOfTheDay.imageUri}}
-        style={styles.photoOfTheDay}
-        resizeMode="cover"/>
-      <Text>
-        {photoOfTheDay.imageTitle}
-      </Text>
-      <Text>
-        {photoOfTheDay.imageDate}
-      </Text>
-      <FlatList 
-        horizontal={true}
-        data={photoOfTheDayList}
-        renderItem={({item}) => (
-          <View style={styles.imageCard}>
-            <Image
-              source={{ uri: item.imageUri }}
-              style={styles.image}
-              resizeMode="cover"
-              keyExtractor={(item, index) => index.toString()}
-            />
-            <Text style={styles.imageDate}>
-              {item.imageDate}
-            </Text>
-          </View>
-        )}
-      />
-        
-      <View style={styles.divider}></View>
-
-      <Text style={styles.heading}>
-        Buscar imagens
-      </Text>
-      <Text>
-        Filtros
-      </Text>
-
-      <View style={styles.divider}></View>
-      
-      <View style={styles.yearArea}>
-        <Pressable style={styles.yearButton}>2025</Pressable>
-        <View style={styles.yearList}>
-          <FlatList 
-          horizontal={true}
-          data={anos}
-          renderItem={({item}) => <Pressable style={styles.yearButton}>{item}</Pressable>}/>
+      <ScrollView >
+        <View style={styles.header}>
+          <Text style={styles.pageName}>Daily Nasa</Text>
         </View>
-      </View>
-      <View style={styles.searchArea}>
-        <TextInput style={styles.searchInput} placeholder='Pesquisar imagens'>
-        </TextInput>
-        <Pressable style={styles.searchButton}>Buscar</Pressable>
-      </View>
-      
-      <Text style={styles.heading}>
-        Resultados da pesquisa
-      </Text>
-      <Text>
-        Mostrando imagens de termo do ano ano
-      </Text>
-      <View>imagens
-        <FlatList 
-        horizontal={true}
-        data={searchPhotos}
-        renderItem={({item}) => (
-          <View style={styles.imageCard}>
-            <Text style={styles.imageTitle}>
-              {item.imageTitle}
+
+        <View style={styles.divider}></View>
+
+        <Text style={styles.heading}>
+          Foto do dia
+        </Text>
+        <View style={styles.contentSection}>
+          <Image
+            source={{ uri: photoOfTheDay.imageUri }}
+            style={styles.photoOfTheDay}
+            resizeMode="cover"/>
+          <Text style={styles.centeredText}>
+            {photoOfTheDay.imageTitle}
+          </Text>
+          <Text style={styles.centeredText}>
+            {photoOfTheDay.imageDate}
+          </Text>
+        </View>
+        <View style={styles.flatListContainer}>
+          <FlatList 
+            horizontal={true}
+            data={photoOfTheDayList}
+            renderItem={({item}) => (
+              <View style={styles.imageCard}>
+                <Image
+                  source={{ uri: item.imageUri }}
+                  style={styles.image}
+                  resizeMode="cover"
+                  keyExtractor={(item, index) => index.toString()}
+                />
+                <Text style={styles.imageDate}>
+                  {item.imageDate}
+                </Text>
+              </View>
+            )}
+          />
+        </View>
+          
+        <View style={styles.divider}></View>
+
+        <Text style={styles.heading}>
+          Buscar imagens
+        </Text>
+        <Text>
+          Filtros
+        </Text>
+
+        <View style={styles.divider}></View>
+        
+        <View style={styles.yearArea}>
+          <Pressable style={styles.yearButton}>
+            <Text style={styles.yearButtonText}>
+              2025
             </Text>
-            <Image
-              source={{ uri: item.imageUri }}
-              style={styles.image}
-              resizeMode="cover"
-              keyExtractor={(item, index) => index.toString()}
-            />
-            <Text style={styles.imageDate}>
-              {item.imageDate}
-            </Text>
-            <Text style={styles.imageDescription}>
-              lorem ipsum dolor sit amet
-            </Text>
+          </Pressable>
+          <View style={styles.yearList}>
+            <FlatList 
+            horizontal={true}
+            data={anos}
+            contentContainerStyle={styles.flatListContainer}
+            renderItem={({item}) => <Pressable style={styles.yearButton}><Text style={styles.yearButtonText}>{item}</Text></Pressable>}/>
           </View>
-        )}
-      />
-      </View>
-      <Text styles={styles.heading}>Desenvolvido por</Text>
+        </View>
 
-      <View style={styles.divider}></View>
+        <View style={styles.divider}></View>
+        
+        <View style={styles.searchArea}>
+          <TextInput 
+            style={styles.searchInput} 
+            placeholder='Pesquisar imagens'>
+          </TextInput>
+          <Pressable style={styles.searchButton}><Text style={styles.searchButtonText}>Buscar</Text></Pressable>
+        </View>
+        
+        <Text style={styles.heading}>
+          Resultados da pesquisa
+        </Text>
 
-      <FlatList 
-        horizontal={true}
-        data={devs}
-        renderItem={({ item }) => (
-          <View>
-            <FontAwesomeIcon icon={faUser}/>
-            <Text>{item.name}</Text>
-            <FontAwesomeIcon icon={faLinkedin}/>
-            <Text style={styles.link} onPress={() => Linking.openURL    (item.linkedin)}>
-              {item.linkedin}
+        <Text style={styles.centeredText}>
+          Mostrando imagens de termo do ano ano
+        </Text>
+
+        <View style={styles.flatListContainer}>
+          <FlatList 
+            horizontal={true}
+            data={searchPhotos}
+            renderItem={({item}) => (
+            <View style={styles.imageCard}>
+              <Text style={styles.imageTitle}>
+                {item.imageTitle}
               </Text>
-            <FontAwesomeIcon icon={faGithub}/><Text>{item.github}</Text>
-          </View>
-        )}
-        keyExtractor={(item, index) => index.toString()}
+              <Image
+                source={{ uri: item.imageUri }}
+                style={styles.image}
+                resizeMode="cover"
+                keyExtractor={(item, index) => index.toString()}
+              />
+              <Text style={styles.imageDate}>
+                {item.imageDate}
+              </Text>
+              <Text style={styles.imageDescription}>
+                lorem ipsum dolor sit amet
+              </Text>
+            </View>
+          )}
         />
+        </View>
+
+        <Text styles={styles.heading}>Desenvolvido por</Text>
+
+        <View style={styles.divider}></View>
+        
+        <View style={styles.flatListContainer}>
+          <FlatList 
+            horizontal={true}
+            data={devs}
+            renderItem={({ item }) => (
+              <View>
+                <FontAwesomeIcon icon={faUser}/>
+                <Text>{item.name}</Text>
+                <FontAwesomeIcon icon={faLinkedin}/>
+                <Text style={styles.link} onPress={() => Linking.openURL    (item.linkedin)}>
+                  {item.linkedin}
+                  </Text>
+                <FontAwesomeIcon icon={faGithub}/><Text>{item.github}</Text>
+              </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#FFFFFF',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    // justifyContent: 'center',
   },
 
-  section: {
-    flex: 1,
+  scrollContent: {
+    alignItems: 'center',
+    paddingVertical: 20,
+    width: '100%',
+  },
+
+  contentSection: {
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 20,
+    marginBottom: 10,
   },
 
   header: {
     fontSize: 20,
+    width: '100%',
+    marginBottom: 10,
   },
 
   pageName: {
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 32,
+    marginBottom: 10,
+    color: '#1C1C1C',
   },
 
   heading: {
     fontSize: 24,
-    textAlign: 'left',
+    textAlign: 'center',
     fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#1C1C1C',
   },
 
   divider: {
     width: '100%',
     height: 1,
     backgroundColor: '#ccc',
-    marginVertical: 10,
+    marginBottom: 10,
+    color: '#0047AB',
   },
 
   photoOfTheDay: {
     width: 300,
     height: 300,
+    marginBottom: 10,
   },  
+
+  flatListContainer: {
+    width: '100%',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
 
   image: {
     width: 150,
@@ -220,12 +276,30 @@ const styles = StyleSheet.create({
   },
 
   imageCard: {
-    flex: 1,
+    alignItems: 'center', 
     rowGap: 2,
+    marginRight: 15, 
+    width: 160,
+    marginBottom: 10,
   },  
 
   imageDate: {
     fontSize: 14,
+    textAlign: 'center',
+    color: '#1C1C1C',
+  },
+
+  imageTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#1C1C1C',
+  },
+
+  imageDescription: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: '#1C1C1C',
   },
 
   photoOfTheDayItem: {
@@ -234,23 +308,37 @@ const styles = StyleSheet.create({
   },
 
   yearList: {
-    //flex: 1,
-    // overflow: 'scroll',
+    flex: 1,
+    width: '100%',
+    justifyContent: 'space-between',
+    marginBottom: 10,
   },
 
   yearButton: {
     borderWidth: 2,
-    textAlign: 'center',
     padding: 6,
-    margin: 4,
+    marginHorizontal: 4, 
     borderRadius: 8,
     borderColor: '#000000',
-    fontSize: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    minWidth: 120,
+    height: 42,
+    marginBottom: 10,
+  },
+
+  yearButtonText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#1C1C1C',
   },
 
   searchArea: {
-    flex: 1,
-    flexDirection: 'row'
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
   },
 
   searchInput: {
@@ -258,13 +346,51 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 6,
     margin: 4,
-    flexGrow: 1
-  }
+    width: '70%',
+    textAlign: 'center',
+  },
 
-  ,searchButton: {
+  searchButton: {
     borderWidth: 2,
     borderRadius: 8,
     padding: 6,
     margin: 4,
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  searchButtonText: {
+    textAlign: 'left',
+    color: '#1C1C1C',
+  },
+
+  devCard: {
+    alignItems: 'center',
+    marginHorizontal: 15,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#eee',
+    borderRadius: 8,
+    width: 150,
+  },
+
+  devName: {
+    marginTop: 5,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#1C1C1C',
+  },
+
+  devLinkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+
+  devLink: {
+    marginLeft: 5,
+    color: '#00f',
+    textDecorationLine: 'underline',
+  },
 });
